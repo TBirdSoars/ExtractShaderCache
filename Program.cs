ef6e454e8827aa2ecb310ae22c70922824f52ec8
TBirdSoars <31261091+TBirdSoars@users.noreply.github.com>
2020-06-20 22:42:07 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace ExtractShaderCache
+{
+    class Program
+    {
+        private byte[] headerBytes = {0x00, 0x03, 0xFE, 0xFF, 0xFE, 0xFF };
+        private byte[] endBytes = {};
+
+        static void Main(string[] args)
+        {
+            if(args.Length != 1)
+            {
+                Console.WriteLine("Please enter exactly one input file.");
+            }
+            else
+            {
+                // Make this do the extracting and writing
+            }
+        }
+    }
+}
